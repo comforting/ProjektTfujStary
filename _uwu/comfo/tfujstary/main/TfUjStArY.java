699443426bfa22ed1ddc1f501a96126c8b84be94
comfo <you@example.com>
2020-12-14 00:48:09 +0100

    Add new files

---
+++ _uwu/comfo/tfujstary/main/TfUjStArY.java
@@ -0,0 +1,11 @@
+package _uwu.comfo.tfujstary.main;
+
+public class TfUjStArY
+{
+    public static void main(final String[] args) throws InterruptedException {
+        Thread.sleep(2500L);
+        System.out.println("Brunetki i blondynki fszystkie was jebac dzifki zyjecie dla pieniedzy wiec umrzecie kurwa w nendzy hcieliscie wyjebac banie bania sra wam na sniadanie wy dziwki jecie moje gufno");
+        Thread.sleep(5999L);
+        System.out.println("FSZYSTKIE WAS JEBAC DZIFKI");
+    }
+}
